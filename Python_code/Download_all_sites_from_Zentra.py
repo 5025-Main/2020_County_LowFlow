b2624eb63c1aa90dd5d56714ea1de7abd564e224
Messina <alex.messina@amecfw.com>
2020-05-26 15:03:45 -0700

    downloaded SWT-235 again

--- Python_code/Download_all_sites_from_Zentra.py
+++ Python_code/Download_all_sites_from_Zentra.py
@@ -25,18 +25,22 @@ if __name__ == "__main__":
     for site_name in site_list['Site'][-1:]:
         print site_name
         
-        ## Get existing data on GitHub to get last data point
-        WL_existing = pd.read_csv('https://raw.githubusercontent.com/5025-Main/2020_County_LowFlow/master/Water_Level_data/'+site_name+'_raw_data_ZentraAPI.csv',index_col=0)
-        # Last existing data point
-        last_data_time = pd.to_datetime(WL_existing.index[-1])
-        print 'Last data point in existing data at: '+last_data_time.strftime("%m/%d/%Y %H:%M")
-        mytz = timezone('US/Pacific')
-        last_data_time_loc = mytz.normalize(mytz.localize(last_data_time,is_dst=True))
-            
-        ## Call Zentra API to get data from last start time to current
-        WL_new = getDeviceReadings(site_name, last_data_time_loc+ dt.timedelta(minutes=5))
-        ## update with new data
-        WL = WL_existing.append(WL_new)
+        try:
+            ## Get existing data on GitHub to get last data point
+            WL_existing = pd.read_csv('https://raw.githubusercontent.com/5025-Main/2020_County_LowFlow/master/Water_Level_data/'+site_name+'_raw_data_ZentraAPI.csv',index_col=0)
+            # Last existing data point
+            last_data_time = pd.to_datetime(WL_existing.index[-1])
+            print 'Last data point in existing data at: '+last_data_time.strftime("%m/%d/%Y %H:%M")
+            mytz = timezone('US/Pacific')
+            last_data_time_loc = mytz.normalize(mytz.localize(last_data_time,is_dst=True))
+                
+            ## Call Zentra API to get data from last start time to current
+            WL_new = getDeviceReadings(site_name, last_data_time_loc+ dt.timedelta(minutes=5))
+            ## update with new data
+            WL = WL_existing.append(WL_new)
+        except:
+            ## Call Zentra API to get data from last start time to current
+            WL = getDeviceReadings(site_name,start_time_loc)
         
         ## Save raw data to csv
         maindir = 'C:/Users/alex.messina/Documents/GitHub/2020_County_LowFlow/'
