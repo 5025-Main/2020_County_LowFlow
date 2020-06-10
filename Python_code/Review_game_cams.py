bf19706064ba158840d6801320f1ea3fe23f3a95
Messina <alex.messina@amecfw.com>
2020-06-10 16:43:18 -0700

    UPDATED BASEFLOW CALCULATION
    so baseflow and quickflow add up

--- Python_code/Review_game_cams.py
+++ Python_code/Review_game_cams.py
@@ -14,7 +14,7 @@ from PIL import Image
 import piexif
 
 
-site_name = 'SDG-072'
+site_name = 'SDG-084'
 
 
 maindir = 'C:/Users/alex.messina/Documents/GitHub/2020_County_LowFlow/'
@@ -64,7 +64,7 @@ print 'datetimes and picture file names....DONE'
 #pics = [os.listdir(pic_dir+pic_folder)][0][5000:] ## You can limit photos here
 
 ## Select by date
-pics = pic_datetimes[pic_datetimes.index >= dt.datetime(2020,5,13,0,0)]['Pic filename']
+pics = pic_datetimes[pic_datetimes.index >= dt.datetime(2020,5,1,0,0)]['Pic filename']
 
 # now the real code :) 
 curr_pos = 0
@@ -137,7 +137,7 @@ def key_event(e):
         
     ax2.xaxis.set_major_formatter(mpl.dates.DateFormatter('%A \n %m/%d/%y %H:%M'))
 
-    ax2.set_ylabel('Flow (gpm)'), ax2_2.set_ylabel('Level (inches)')
+    ax2.set_ylabel('Flow (gpm)',color='w'), ax2_2.set_ylabel('Level (inches)',color='w')
 
     ## Legends, they're all around
     ax2.legend(loc='upper left')
@@ -149,7 +149,7 @@ def key_event(e):
 
 
 fig1, (ax1,ax2) = plt.subplots(2,1,figsize=(16,11),gridspec_kw={'height_ratios': [2, 1]})
-
+fig1.patch.set_facecolor('#000000')
 fig1.canvas.mpl_connect('key_press_event', key_event)
 
 
@@ -200,7 +200,7 @@ ax2.legend(loc='upper left')
 ax2_2.legend(loc='upper right')
 
 ## Y labels
-ax2.set_ylabel('Flow (gpm)'), ax2_2.set_ylabel('Level (inches)')
+ax2.set_ylabel('Flow (gpm)',color='w',fontsize=11,fontweight='bold'), ax2_2.set_ylabel('Level (inches)',color='w',fontsize=11,fontweight='bold')
 
 
 ## x and y limits
@@ -215,5 +215,12 @@ else:
 ## X axis date format
 ax2.xaxis.set_major_formatter(mpl.dates.DateFormatter('%A \n %m/%d/%y %H:%M'))
 
+ax1.spines['bottom'].set_color('w')
+ax1.spines['top'].set_color('w')
+ax1.xaxis.label.set_color('w')
+ax1.yaxis.label.set_color('w')
+ax2.yaxis.label.set_color('w')
+ax1.tick_params(axis='x', colors='w')
+ax2.tick_params(axis='x', colors='w')
 
 plt.tight_layout()
