383b652f6e50f3ab074ec4fc40a40d46c4d3ac95
Messina <alex.messina@amecfw.com>
2020-06-10 22:20:10 -0700

    removed global offset

--- Python_code/Review_game_cams.py
+++ Python_code/Review_game_cams.py
@@ -15,7 +15,7 @@ import piexif
 
 
 site_name = 'SLR-045'
-pic_start_time = dt.datetime(2020,5,1,0,0)
+pic_start_time = dt.datetime(2020,5,22,0,0)
 
 maindir = 'C:/Users/alex.messina/Documents/GitHub/2020_County_LowFlow/'
 
