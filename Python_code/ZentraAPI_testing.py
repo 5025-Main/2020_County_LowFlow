3b94041142146e33fb5bc3f3e0f91b0561b7d44f
Messina <alex.messina@amecfw.com>
2020-07-07 16:10:46 -0700

    sdr204a being weird

--- Python_code/ZentraAPI_testing.py
+++ Python_code/ZentraAPI_testing.py
@@ -19,7 +19,7 @@ start_time_loc = mytz.normalize(mytz.localize(start_time_loc,is_dst=True))
 ## Get Master Site List
 site_list = pd.read_csv('https://raw.githubusercontent.com/5025-Main/2020_County_LowFlow/master/Ancillary_files/MasterSiteList.csv')
 ## Just one site
-site_list =  site_list[site_list['Site'] == 'SLR-095'] ###########
+site_list =  site_list[site_list['Site'] == 'SDR-204A'] ###########
 
 #site_list = site_list[~site_list['Site'].isin(['SLR-095','SDG-084','SDR-098','SDG-085M'])]
 
