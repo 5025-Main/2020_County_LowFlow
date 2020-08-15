4bc7cb7730e2f4121422dfaeaebbc7901ceea8c2
Messina <alex.messina@amecfw.com>
2020-08-14 17:30:41 -0700

    offset SLR-045

--- Python_code/Review_game_cams.py
+++ Python_code/Review_game_cams.py
@@ -13,8 +13,8 @@ from scipy import ndimage
 from PIL import Image
 import piexif
 
-site_name = 'SLR-095'
-pic_start_time = dt.datetime(2020,7,20,0,0)
+site_name = 'SLR-045'
+pic_start_time = dt.datetime(2020,7,1,0,0)
 
 maindir = 'C:/Users/alex.messina/Documents/GitHub/2020_County_LowFlow/'
 ## Local file
