4a0c0549b3bff0232862793311dbf994dba779f1
Alex Messina <atm1984@gmail.com>
2020-09-11 09:56:20 -0700

    draft SDR-041

--- Python_code/Review_game_cams.py
+++ Python_code/Review_game_cams.py
@@ -13,8 +13,8 @@ from scipy import ndimage
 from PIL import Image
 import piexif
 
-site_name = 'SDG-085'
-pic_start_time = dt.datetime(2020,7,31,0,0)
+site_name = 'SDR-036'
+pic_start_time = dt.datetime(2020,7,31,22,0)
 
 maindir = 'C:/Users/alex.messina/Documents/GitHub/2020_County_LowFlow/'
 ## Local file
