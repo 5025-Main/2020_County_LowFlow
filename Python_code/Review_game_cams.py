935a1345015afbaee86602b703e53159b5550ebf
Alex Messina <atm1984@gmail.com>
2020-09-15 19:59:54 -0700

    swt-030 draft

--- Python_code/Review_game_cams.py
+++ Python_code/Review_game_cams.py
@@ -13,8 +13,8 @@ from scipy import ndimage
 from PIL import Image
 import piexif
 
-site_name = 'SWT-019'
-pic_start_time = dt.datetime(2020,8,24,0,0)
+site_name = 'SWT-030'
+pic_start_time = dt.datetime(2020,8,1,0,0)
 
 maindir = 'C:/Users/alex.messina/Documents/GitHub/2020_County_LowFlow/'
 ## Local file
